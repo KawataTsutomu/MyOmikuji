51aca9bc035ef6da38d88ba60d77d674741cf948
KawataTsutomu <tomtom.net1@gmail.com>
2021-11-01 17:00:19 +0900

    ボタンのクリックイベント追加

--- js/main.js
+++ js/main.js
@@ -4,6 +4,23 @@
     const btn = document.getElementById('btn');
 
     btn.addEventListener('click', () => {
-        btn.textContent = 'hit!';
+        const results = ["大吉", "中吉", "吉", "凶"];
+        const n = Math.floor(Math.random() * results.length);
+        btn.textContent = results[n];
+
+        // switch (n) {
+        //     case 0:
+        //         btn.textContent = "大吉";
+        //         break;
+        //     case 1:
+        //         btn.textContent = "中吉";
+        //         break;
+        //     case 2:
+        //         btn.textContent = "吉";
+        //         break;
+        //     case 3:
+        //         btn.textContent = "凶";
+        //         break;
+        // }
     });
 }
